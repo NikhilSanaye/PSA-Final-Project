d96d10eafee4323c2cd973db46ddbfd0013b8290
Nikhil Sanaye <sanaye.ni@northeastern.edu>
2022-04-26 19:55:53 -0400

    project

---
+++ src/main/java/com/project/StringScore.java
@@ -0,0 +1,19 @@
+package com.project;
+
+public class StringScore {
+	String counterMove;
+	int score;
+	public String getCounterMove() {
+		return counterMove;
+	}
+	public void setCounterMove(String s) {
+		this.counterMove = s;
+	}
+	public int getScore() {
+		return score;
+	}
+	public void setScore(int score) {
+		this.score = score;
+	}
+	
+}
